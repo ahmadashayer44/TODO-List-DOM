fb43f8d336e33c335a6812f772fb15c8f8c8ca35
ahmadashayer44 <ashayera44@gmail.com>
2025-08-04 17:33:47 +0300

    implement UI/UX design for todo task interface

---
+++ JS/TODO.js
@@ -0,0 +1,10 @@
+let button1 = document.createElement("button");
+let button2 = document.createElement("button");
+
+button1.innerHTML = "Delete";
+button2.innerHTML = "Done";
+
+button1.classList.add("delete-button");
+button2.classList.add("done-button");
+
+d;
---
+++ JS/config.js
@@ -0,0 +1,1 @@
+export const TODO_API = "https://dummyjson.com/todos";

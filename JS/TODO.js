ec4f20cbd44b8ab9fe1e71b749aadc56b40dfad1
ahmadashayer44 <ashayera44@gmail.com>
2025-08-04 19:35:39 +0300

    Retreiving TODO's form API, Counting TODO's and Display it

--- JS/TODO.js
+++ JS/TODO.js
@@ -1,10 +1,57 @@
+import { TODO_API } from "./config.js";
+
 let button1 = document.createElement("button");
 let button2 = document.createElement("button");
-
+let totalTask = document.getElementById("total-tasks");
+let totalTaskCounter = 0;
 button1.innerHTML = "Delete";
 button2.innerHTML = "Done";
 
 button1.classList.add("delete-button");
 button2.classList.add("done-button");
 
-d;
+let TODOS = await fetch(TODO_API).then((res) =>
+  res.json().then((data) => data.todos)
+);
+
+console.log(TODOS);
+function addTodo(todo) {
+  let todoRow = document.createElement("tr");
+  let todoID = document.createElement("td");
+  let todoDescription = document.createElement("td");
+  let todoUserID = document.createElement("td");
+  let todoStatus = document.createElement("td");
+  let todoActions = document.createElement("td");
+
+  let button1 = document.createElement("button");
+  let button2 = document.createElement("button");
+  button1.innerHTML = "Delete";
+  button2.innerHTML = "Done";
+  button1.classList.add("delete-button");
+  button2.classList.add("done-button");
+
+  todoActions.style.display = "flex";
+  todoActions.style.flexDirection = "row";
+  todoActions.style.justifyContent = "space-between";
+  todoActions.style.gap = "15px";
+
+  todoID.innerHTML = todo.id;
+  todoDescription.innerHTML = todo.todo;
+  todoUserID.innerHTML = todo.userId;
+  todoStatus.innerHTML = todo.completed;
+
+  todoActions.appendChild(button1);
+  todoActions.appendChild(button2);
+
+  todoRow.appendChild(todoID);
+  todoRow.appendChild(todoDescription);
+  todoRow.appendChild(todoUserID);
+  todoRow.appendChild(todoStatus);
+  todoRow.appendChild(todoActions);
+
+  document.getElementById("todo-tbody").appendChild(todoRow);
+  totalTaskCounter++;
+  totalTask.innerHTML = totalTaskCounter;
+}
+
+TODOS.forEach(addTodo);
